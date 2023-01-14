70fb8f9a395dc8fbcccaf0143409d4a41c30d552
2298269347@qq.com <2298269347@qq.com>
2023-01-14 10:06:10 +0800

    邮件celery异步发送

--- celery_tasks/email/tasks.py
+++ celery_tasks/email/tasks.py
@@ -4,18 +4,21 @@ from celery_tasks.main import app
 from meiduo_mall import settings
 
 
-@app.task
-def send_active_email(email):
+@app.task(bind=True)
+def send_active_email(self, email):
     # 发送激活邮件
     subject = '主题'
     message = "内容"
     from_email = settings.EMAIL_FROM
     html_message = '<h1>哈哈哈哈和</h1>'
     recipient_list = [email]
-    send_mail(
-        subject=subject,
-        message=message,
-        from_email=from_email,
-        recipient_list=recipient_list,
-        html_message=html_message
-    )
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            html_message=html_message
+        )
+    except Exception as e:
+        self.retry(exc=e)
